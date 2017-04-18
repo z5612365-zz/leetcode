e51b32b60fe0290f5d13bfb0a205621c3532cb00
Chang-Yung Chi <z5612365@gmail.com>
2017-04-18 23:03:24 +0800

    Update 557_Reverse_Words_in_a_String_III.java

--- algorithms/java/easy/557_Reverse_Words_in_a_String_III.java
+++ algorithms/java/easy/557_Reverse_Words_in_a_String_III.java
@@ -1,1 +1,15 @@
+public class Solution {
+    public String reverseWords(String s) {
+        
+        String[] splited = s.split("\\s+");
+        String result="";
+        for(int i=0;i<splited.length;i++){
+            result+=new StringBuilder(splited[i]).reverse().toString();
+            result+=" ";
+        }
+        result=result.substring(0, result.length() - 1); //remove the last space
 
+        //System.out.println(result);
+        return result;
+    }
+}
